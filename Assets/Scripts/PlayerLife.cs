54a005ce8fce642337f6f57c83044b3714d2c83d
ErenAksitElam <EAksit2024@elam.co.uk>
2024-11-14 12:43:56 +0000

    Stuff 200 or something

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -9,20 +10,20 @@ public class PlayerLife : MonoBehaviour
 
     bool dead = false;
 
-    private void Update()
-    {
-        if (transform.position.y < -1f && !dead)
-        {
-            Die();
-        }
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy Body"))
         {
             GetComponent<MeshRenderer>().enabled = false;
             GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            Die();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DeathZone"))
+        {
             GetComponent<PlayerMovement>().enabled = false;
             Die();
         }
